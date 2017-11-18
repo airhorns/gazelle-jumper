009e8e580e760c3f41a120c113c8864ba22eca69
Harry Brundage <harry.brundage@gmail.com>
2017-11-18 11:45:03 -0500

    Faster!

--- config/schedule.rb
+++ config/schedule.rb
@@ -3,7 +3,7 @@ set :output, "~/gazelle-jumper/shared/log/cron_log.log"
 job_type :rbenv_command, %Q{export PATH=/home/deploy/.rbenv/shims:/home/deploy/.rbenv/bin:/usr/bin:$PATH; eval "$(rbenv init -)"; \
                          cd :path && :environment_variable=:environment :bundle_command :task :output }
 
-every 5.minutes do
+every 1.minutes do
   rbenv_command "ruby log_torrents.rb"
 end
 
